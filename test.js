f080ed0e3ea5c2de15fe7e2fa4239c75dca5fb15
elgato <amzfba.1bestbuy@gmail.com>
2022-04-28 19:39:58 +0200

    external js file

---
+++ test.js
@@ -0,0 +1,28 @@
+
+
+
+$(document).ready(function () {
+  // jQuery methods go here...
+});
+
+$("p");
+
+$(document).ready(function () {
+  $("button").click(function () {
+    $("p").hide();
+  });
+});
+
+$("#test");
+
+$(document).ready(function () {
+  $("button").click(function () {
+    $("#test").hide();
+  });
+});
+
+$(document).ready(function () {
+  $("button").click(function () {
+    $(".test").hide();
+  });
+});
